58df0b0d182067c644b57320062e1d7afc24bf3e
lindu <linduoracle@gmail.com>
2026-09-12 23:49:42 +0800

    modify

--- src/main/java/com/wsy/mapper/OperatorMapper.java
+++ src/main/java/com/wsy/mapper/OperatorMapper.java
@@ -1,16 +1,26 @@
 package com.wsy.mapper;
+
 import com.wsy.model.Operator;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+
 import java.util.List;
+
 @Mapper
 public interface OperatorMapper {
     Operator login(@Param("userName") String userName, @Param("password") String password);
+
     int insert(Operator operator);
+
     int update(Operator operator);
+
     int deleteById(Integer id);
+
     Operator selectById(Integer id);
+
     int updatePassword(@Param("id") Integer id, @Param("newPassword") String newPassword);
+
     List<Operator> selectAll();
+
     List<Operator> selectByCondition(@Param("column") String column, @Param("value") String value);
 }
--- src/main/java/com/wsy/mapper/OrderMapper.java
+++ src/main/java/com/wsy/mapper/OrderMapper.java
@@ -1,20 +1,32 @@
 package com.wsy.mapper;
+
 import com.wsy.model.Order;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
+
 @Mapper
 public interface OrderMapper {
     List<Order> selectAll();
+
     Order selectById(Integer id);
+
     List<Order> selectByBookId(Integer bookId);
+
     List<Order> selectByOperatorId(Integer operatorId);
+
     int insert(Order order);
+
     int update(Order order);
+
     int delete(Integer id);
+
     List<Map<String, Object>> selectUnacceptedOrders();
+
     int updateOrderAcceptStatus(@Param("bookISBN") String bookISBN, @Param("orderDate") LocalDate orderDate);
+
     int deleteUnacceptedOrder(@Param("bookISBN") String bookISBN, @Param("orderDate") LocalDate orderDate);
 }
